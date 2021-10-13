29b48de6881f95c37ce553b6afd9fcacc1838ff4
arthris <oclockth@gmail.com>
2021-10-13 18:37:38 +0700

    Task 2 complete

--- SP_HOMEWORK/src/main/java/creational/BookMetadataExporter.java
+++ SP_HOMEWORK/src/main/java/creational/BookMetadataExporter.java
@@ -1,10 +1,23 @@
 package creational;
 
+import javax.xml.parsers.ParserConfigurationException;
+import java.io.IOException;
 import java.io.PrintStream;
 
 public abstract class BookMetadataExporter extends BookCollection {
 
+    BookMetadataFormatter bookFormatter;
+    {
+        try {
+            bookFormatter = createFormat();
+        } catch (ParserConfigurationException | IOException e) {
+            e.printStackTrace();
+        }
+    }
     public void export(PrintStream stream) {
         // Please implement this method. You may create additional methods as you see fit.
+        stream.print(bookFormatter.getMetadataString());
     }
+
+    public abstract BookMetadataFormatter createFormat() throws ParserConfigurationException, IOException;
 }
